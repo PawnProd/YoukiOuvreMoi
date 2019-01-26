f94725c0cda572c984dd16c7bbc1a063985c9763
Allemand Guillaume <allemandg.contact@gmail.com>
2019-01-26 08:48:34 +0100

    Suppression Examinable

--- Assets/Scripts/Objects/Object.cs
+++ Assets/Scripts/Objects/Object.cs
@@ -9,10 +9,25 @@ public class Object : MonoBehaviour
     public ObjectSize size;
     public Vector3 location;
 
+    public bool open;
+    public GameObject key;
+
+    public Object containedObject;
+    
+    private bool pushable;
+    private bool movable;
+    private bool grabable;
+    private bool examinable;
+
+    public Node currentNode;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pushable = weight != ObjectWeight.Unmovable;
+        movable = weight == ObjectWeight.Light || weight == ObjectWeight.Medium;
+        grabable = weight != ObjectWeight.Unmovable;
+        examinable = weight != ObjectWeight.Light || weight != ObjectWeight.Medium || !open;
     }
 
     // Update is called once per frame
@@ -20,6 +35,23 @@ public class Object : MonoBehaviour
     {
         
     }
+
+    public bool BeingPushed ()
+    {
+        if (pushable)
+        {
+            bool actionSuccessful = false;
+
+            // TODO
+
+
+            return actionSuccessful;
+        } else {
+            return pushable;
+        }
+    }
+
+
 
 
 
