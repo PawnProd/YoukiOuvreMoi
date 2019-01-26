a52d0d4144d249726d4ec8819fff5bff8604f864
pierrebigaud <pierrebigaud44@orange.fr>
2019-01-26 21:12:26 +0100

    testlevel1-2

--- Assets/Scripts/GridSystem.cs
+++ Assets/Scripts/GridSystem.cs
@@ -23,6 +23,7 @@ public class GridSystem : MonoBehaviour
         BoundsInt tilemapBounds = tileMap.cellBounds;
         TileBase[] allTiles = tileMap.GetTilesBlock(tilemapBounds);
         grid = new Node[tilemapBounds.size.x, tilemapBounds.size.y];
+        Debug.Log("X = " + tilemapBounds.size.x + " Y = " + tilemapBounds.size.y);
         for (int x = 0; x < tilemapBounds.size.x; x++)
         {
             for (int y = 0; y < tilemapBounds.size.y; y++)
