28e19f368e776bc7f6b99d906cecf4f98f812ddb
Allemand Guillaume <allemandg.contact@gmail.com>
2019-01-26 20:26:06 +0100

    Victory

---
+++ Assets/Scripts/Menu/VictoryController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class VictoryController : MonoBehaviour
+{
+    public Transform victoryStats;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        majStats();
+    }
+
+    public void majStats()
+    {
+        victoryStats.Find("NbConfused").GetComponent<TextMeshProUGUI>().text = ""+GameController.Instance.dog.nbOfConfusedTimes;
+    }
+}
